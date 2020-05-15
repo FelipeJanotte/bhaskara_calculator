722ad8778a8be54edfa60abc30de207169e96e84
Felipe <f.a.janotte@gmail.com>
2020-05-15 17:42:01 -0300

    chore: Funcao

---
+++ js.js
@@ -0,0 +1,23 @@
+function BHASKARA(A, B, C) {
+
+  //DECLARING THE VARIABLES
+  var X1 = 0
+  var DELTA = 0
+  var X2 = 0
+
+  //DELTA EQUATION
+  DELTA = B ^ 2 - 4 * A * C
+  
+  //BHASKARA EQUATION
+  if (DELTA = 0){
+    X1 = (-B + Math.sqrt(DELTA)) / 2 *  A
+  }
+
+  if (DELTA != 0){
+    X1 = (-B + Math.sqrt(DELTA)) / 2 * A
+    X2 = (-B + Math.sqrt(DELTA)) / 2 * A
+  } 
+  
+  //PASS VARIABLES TO HTML
+
+}
