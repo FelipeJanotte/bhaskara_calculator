3baf35ff0fd87c99853feb2e9a86ab53d0c5671a
Felipe <f.a.janotte@gmail.com>
2020-05-19 15:19:23 -0300

    fix: Arrumando calculo

--- js.js
+++ js.js
@@ -1,23 +1,22 @@
-function BHASKARA(A, B, C) {
+function BHASKARA(A , B , C) {
 
   //DECLARING THE VARIABLES
-  var X1 = 0
-  var DELTA = 0
-  var X2 = 0
+  var X1 ;
+  var DELTA ; 
+  var X2 ;
 
   //DELTA EQUATION
-  DELTA = B ^ 2 - 4 * A * C
+  DELTA = Math.pow(B , 2) - (4*A*C );
+  console.log(DELTA)
+  document.getElementById("DELTA").innerHTML = "O valor de DELTA(Δ) é: " + DELTA;
   
   //BHASKARA EQUATION
-  if (DELTA = 0){
-    X1 = (-B + Math.sqrt(DELTA)) / 2 *  A
-  }
+    X1 = (-B+Math.sqrt(DELTA))/2*A;
+    console.log(X1)
+    X2 = (-B -Math.sqrt(DELTA))/2*A;
+    console.log(X2)
 
-  if (DELTA != 0){
-    X1 = (-B + Math.sqrt(DELTA)) / 2 * A
-    X2 = (-B + Math.sqrt(DELTA)) / 2 * A
-  } 
-  
-  //PASS VARIABLES TO HTML
-
+document.getElementById("X1").innerHTML = "O valor de X' é: " + X1;
+document.getElementById("X2").innerHTML = "O valor de X'' é: " + X2;
+//PASS VARIABLE TO HTML
 }
